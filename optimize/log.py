b6d6dc256373d19214fa236b351fb25a9c8d20b6
Irina <irlania@mail.ru>
2019-09-20 16:34:26 +0300

    teardown, path.join, history

--- optimize/log.py
+++ optimize/log.py
@@ -1,12 +1,15 @@
+import os
+
+
 def clean_file(path, name='file.txt'):
-    with open(path+name, 'w') as file:
+    with open(os.path.join(path, name), 'w') as file:
         file.seek(0)
 
 
 def write_to_file(path, i, list_of_mins, list_of_averages, global_min, global_avg, name='file.txt'):
-    with open(path+name, 'a') as file:
+    with open(os.path.join(path, name), 'a') as file:
         file.write(f'History {i}\n'),
-        file.write(f'Минимумы всех поколений: {list_of_mins}\n')
-        file.write(f'Средние всех поколений: {list_of_averages}\n')
-        file.write(f'Глобальный минимум: {global_min}\n')
-        file.write(f'Глобальное среднее: {global_avg}\n')
+        file.write(f'Mins through the history: {list_of_mins}\n')
+        file.write(f'Avgs through the history: {list_of_averages}\n')
+        file.write(f'Global min: {global_min}\n')
+        file.write(f'Global avg: {global_avg}\n')
--- tests/test_optimization.py
+++ tests/test_optimization.py
@@ -6,7 +6,7 @@ import time
 
 
 @pytest.fixture()
-def t_generation():
+def generation_for_test():
     return [(3, 7), (9, 4), (10, 4), (5, 2), (7, 2), (7, 5), (8, 2), (8, 7), (2, 2), (6, 7)]
 
 
@@ -14,32 +14,32 @@ def test_fitness_correct():
     assert optimisation.fitness((1, 3)) == 10
 
 
-def test_mutation_correct(t_generation):
+def test_mutation_correct(generation_for_test):
     random.seed(1)
     delta = 10 ** (-3)
     sign = random.choice([-1, 1])
-    parent = random.choice(t_generation)
+    parent = random.choice(generation_for_test)
     child = (parent[0] + sign * delta, parent[1] + sign * delta)
-    assert optimisation.mutation(t_generation, delta, random_seed=1) == child
+    assert optimisation.mutation(generation_for_test, delta, random_seed=1) == child
 
 
-def test_crossover_correct(t_generation):
+def test_crossover_correct(generation_for_test):
     random.seed(1)
-    parent_1 = random.choice(t_generation)
-    parent_2 = random.choice(t_generation)
-    assert optimisation.crossover(t_generation, random_seed=1) == (parent_1[0], parent_2[1])
+    parent_1 = random.choice(generation_for_test)
+    parent_2 = random.choice(generation_for_test)
+    assert optimisation.crossover(generation_for_test, random_seed=1) == (parent_1[0], parent_2[1])
 
 
 def test_init_generation_correct():
     assert len(optimisation.init_generation(10)) == 10
 
 
-def test_new_offspring(t_generation):
+def test_new_offspring(generation_for_test):
     delta = 10 ** (-3)
-    retain_num = int(len(t_generation) * 0.2)
-    crossover_num = int(len(t_generation) * 0.4)
-    mutation_num = int(len(t_generation) * 0.4)
-    assert len(optimisation.new_offspring(t_generation, retain_num, crossover_num, mutation_num, delta)) == 10
+    retain_num = int(len(generation_for_test) * 0.2)
+    crossover_num = int(len(generation_for_test) * 0.4)
+    mutation_num = int(len(generation_for_test) * 0.4)
+    assert len(optimisation.new_offspring(generation_for_test, retain_num, crossover_num, mutation_num, delta)) == 10
 
 
 def test_convert_data_to_dataframe():
